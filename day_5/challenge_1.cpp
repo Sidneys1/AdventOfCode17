7ad4dda1c54fe50191926d1e73f2a4a18ed7164b
Sidney Borne <sidneys1@live.com>
2017-12-06 20:19:58 -0500

    Day 5 done

---
+++ day_5/challenge_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int /* argc */, char * /* argv */ []) {
+    std::vector<int> trampoline;
+    {
+        std::string line;
+        do {
+            std::getline(std::cin, line);
+            if (line.empty()) {
+                break;
+            }
+            trampoline.push_back(std::stoi(line));
+        } while (true);
+    }
+
+    std::cout << "There are " << trampoline.size() << " trampoline entries."
+              << std::endl;
+
+    int position = 0, moves = 0, max = trampoline.size();
+
+    do {
+        int dist = trampoline[position]++;
+        position += dist;
+        moves++;
+    } while (position >= 0 && position < max);
+
+    std::cout << "Completed in " << moves << " moves." << std::endl;
+
+    return 0;
+}
