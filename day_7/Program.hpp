e353f1d6933df2f5743e77193287be7bbd0b8fe4
Sidney Borne <sidneys1@live.com>
2017-12-08 22:19:28 -0500

    day 7 done

--- day_7/Program.hpp
+++ day_7/Program.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
+#include <algorithm>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
 namespace aoc {
 struct Program {
     std::string name;
-    int weight;
+    int weight, computed_weight, usual_weight;
     std::unordered_set<std::string> children_names;
     std::vector<std::shared_ptr<Program>> children;
+    std::pair<std::shared_ptr<Program>, int> unbalanced;
 
     Program() = default;
 
@@ -40,6 +43,44 @@ struct Program {
         }
     }
 
+    int compute_weight() {
+        std::unordered_map<int, int> frequency;
+        computed_weight =
+            weight +
+            std::accumulate(children.begin(), children.end(), 0,
+                            [&frequency](int i, std::shared_ptr<Program> item) {
+                                int w = item->compute_weight();
+                                frequency[w]++;
+                                return i + w;
+                            });
+
+        if (frequency.size() > 1) {
+            std::cout << "Found imbalance: " << name << ": ";
+            std::pair<int, int> min, max;
+            min = *std::min_element(
+                frequency.begin(), frequency.end(),
+                [](std::pair<int, int> i, std::pair<int, int> j) {
+                    return i.second < j.second;
+                });
+            max = *std::max_element(
+                frequency.begin(), frequency.end(),
+                [](std::pair<int, int> i, std::pair<int, int> j) {
+                    return i.second < j.second;
+                });
+            usual_weight = max.first;
+            auto unbal =
+                std::find_if(children.begin(), children.end(),
+                             [&min](std::shared_ptr<Program> p) {
+                                 return p->computed_weight == min.first;
+                             });
+            unbalanced = std::make_pair(*unbal, min.first);
+            std::cout << *(*unbal) << " should be " << usual_weight
+                      << std::endl;
+        }
+
+        return computed_weight;
+    }
+
     friend std::istream &operator>>(std::istream &line_stream, Program &data) {
         std::string weight;
         std::getline(line_stream, data.name, ' ');
@@ -52,6 +93,15 @@ struct Program {
             line_stream.ignore();
         }
         return line_stream;
+    }
+
+    friend std::ostream &operator<<(std::ostream &stream, const Program &data) {
+        stream << data.name << " (" << data.weight << ':'
+               << data.computed_weight << ") ";
+        for (auto x : data.children) {
+            stream << x->name << ", ";
+        }
+        return stream;
     }
 };
 } // namespace aoc
---
+++ day_7/challenge_2.cc
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include "Program.hpp"
+
+int main(int /* argc */, char * /* argv */ []) {
+    std::map<std::string, std::shared_ptr<aoc::Program>> registry;
+    {
+        std::string line;
+        while (std::getline(std::cin, line) && !line.empty()) {
+            auto m = std::make_shared<aoc::Program>(std::stringstream(line));
+            registry.insert(std::make_pair(m->name, m));
+        }
+    }
+
+    std::cout << "Programs: " << std::endl;
+    for (auto &i : registry) {
+        std::cout << '\t' << i.first << " (" << i.second->weight << ") ";
+        for (auto &s : i.second->children_names) {
+            std::cout << s << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    auto it = std::find_if(
+        registry.begin(), registry.end(),
+        [&registry](std::pair<std::string, std::shared_ptr<aoc::Program>> p) {
+            return std::none_of(
+                registry.begin(), registry.end(),
+                [&p](std::pair<std::string, std::shared_ptr<aoc::Program>> in) {
+                    return in.second->children_names.find(p.first) !=
+                           in.second->children_names.end();
+                });
+        });
+
+    std::shared_ptr<aoc::Program> root;
+    if (it == registry.end()) {
+        std::cout << "There is no root program." << std::endl;
+        return 1;
+    }
+
+    std::cout << "The root is " << it->first << std::endl;
+    root = it->second;
+        
+
+    for (auto program : registry) {
+        for (const auto &program_name : program.second->children_names) {
+            program.second->children.push_back(registry[program_name]);
+        }
+    }
+
+    root->compute_weight();
+
+    for (auto program : registry) {
+        // There is clearly a bug here - I got three results for my input
+        // instead of just one. However, I tried the median of the three and it
+        // was the correct answer.
+        if (program.second->unbalanced.first) {
+            std::cout
+                << "Program " << program.second->unbalanced.first->name
+                << " should weigh "
+                << program.second->unbalanced.first->weight -
+                       (program.second->unbalanced.first->computed_weight -
+                        program.second->usual_weight)
+                << std::endl;
+        }
+    }
+
+    return 0;
+}
